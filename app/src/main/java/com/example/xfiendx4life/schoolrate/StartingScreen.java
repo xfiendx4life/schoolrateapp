b431539fb575738bbaf32d54881fbf3163167a6e
Kirill Ivanov <xfiendx4life@gmail.com>
2017-08-09 20:49:54 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/xfiendx4life/schoolrate/StartingScreen.java
@@ -0,0 +1,13 @@
+package com.example.xfiendx4life.schoolrate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starting_screen);
+    }
+}

8467f5720892d91e543637fe7a2b7f5fc9986928
xfiendx4life <xfiendx4life@gmail.com>
2017-08-10 12:26:24 +0300

    Added MainAppScreenActivity

--- app/src/main/java/com/example/xfiendx4life/schoolrate/StartingScreen.java
+++ app/src/main/java/com/example/xfiendx4life/schoolrate/StartingScreen.java
@@ -1,7 +1,9 @@
 package com.example.xfiendx4life.schoolrate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartingScreen extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class StartingScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting_screen);
+    }
+    public void changeScreen (View view) {
+        Intent intent = new Intent(this, MainAppScreenActivity.class);
+        startActivity(intent);
     }
 }

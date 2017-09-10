12c16fcf773724b3d569d9f2735c1a30b290a09d
xfiendx4life <xfiendx4life@gmail.com>
2017-09-10 23:46:57 +0300

    Получены данные из алфавитного списка

--- app/src/main/java/com/example/xfiendx4life/schoolrate/Alphabet.java
+++ app/src/main/java/com/example/xfiendx4life/schoolrate/Alphabet.java
@@ -1,16 +1,41 @@
 package com.example.xfiendx4life.schoolrate;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import static com.example.xfiendx4life.schoolrate.htmlParser.FullSchoolList;
 
 public class Alphabet extends AppCompatActivity {
+
+    ArrayList<School> schools;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alphabet);
+        new GetData().execute();
     }
-
+    private  class GetData extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                schools = FullSchoolList("http://www.schoolrate.ru/school_list.html");
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        return null;
+        }
+        @Override
+        protected void onPostExecute (Void result) {
+            TextView test = (TextView) findViewById(R.id.test);
+            test.setText(schools.get(5).schoolName);
+        }
+    }
     public  void fillAlphabet() {
 
     }

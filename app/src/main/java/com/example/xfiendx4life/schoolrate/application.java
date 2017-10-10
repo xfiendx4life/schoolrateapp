1ffe4ee4ca749e560fa1558fda07900a71c09c22
xfiendx4life <xfiendx4life@gmail.com>
2017-10-10 20:59:40 +0300

    Добавлена отправка тестовой заявки через приложение

--- app/src/main/java/com/example/xfiendx4life/schoolrate/application.java
+++ app/src/main/java/com/example/xfiendx4life/schoolrate/application.java
@@ -1,7 +1,13 @@
 package com.example.xfiendx4life.schoolrate;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class application extends AppCompatActivity {
 
@@ -9,5 +15,39 @@ public class application extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_application);
+        Button sendButton = (Button) findViewById(R.id.send_button);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //readDataFromEdits();
+                sendEmail();
+            }
+        });
+    }
+
+    protected void readDataFromEdits() {
+        //читаем данные из editов
+    }
+
+    protected void sendEmail () {
+        Log.i("Send email","");
+        String[] TO = {"xfiendx4life@schoolrate.ru"};
+        String[] CC = {""};
+
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            finish();
+            Log.i("Finished sending email", "");
+        }
+        catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(application.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
     }
 }

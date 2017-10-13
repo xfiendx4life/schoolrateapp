2797f79f397c9fb75d0c939e0322a55b7fd1e5b6
xfiendx4life <xfiendx4life@gmail.com>
2017-10-13 12:51:43 +0300

    Добавлена отправка тестовой заявки через приложение с текстом

--- app/src/main/java/com/example/xfiendx4life/schoolrate/application.java
+++ app/src/main/java/com/example/xfiendx4life/schoolrate/application.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class application extends AppCompatActivity {
@@ -30,6 +31,10 @@ public class application extends AppCompatActivity {
     }
 
     protected void sendEmail () {
+        EditText name = (EditText) findViewById(R.id.user_name_edit);
+        EditText email = (EditText) findViewById(R.id.email_edit);
+        EditText phone = (EditText) findViewById(R.id.phone_edit);
+        EditText extra = (EditText) findViewById(R.id.extra);
         Log.i("Send email","");
         String[] TO = {"xfiendx4life@schoolrate.ru"};
         String[] CC = {""};
@@ -39,8 +44,11 @@ public class application extends AppCompatActivity {
         emailIntent.setType("text/plain");
         emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
         emailIntent.putExtra(Intent.EXTRA_CC, CC);
-        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
-        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Подбор школы");
+        String emailText = new String();
+        emailText = String.format("Имя: %s \n Телефон: %s \n Электронный адрес: %s \n Пожелания по обучению: \n %s ",
+                name.getText(),email.getText(),phone.getText(),extra.getText());
+        emailIntent.putExtra(Intent.EXTRA_TEXT, emailText);
         try {
             startActivity(Intent.createChooser(emailIntent, "Send mail..."));
             finish();

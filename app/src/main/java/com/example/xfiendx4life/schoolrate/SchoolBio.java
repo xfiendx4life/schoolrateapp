a131ba533a41841699ec3243064f06fe42e50399
xfiendx4life <xfiendx4life@gmail.com>
2017-10-17 14:42:49 +0300

    Активити с описанием школы. Добавлена кнопка. Но нужна ли она??

--- app/src/main/java/com/example/xfiendx4life/schoolrate/SchoolBio.java
+++ app/src/main/java/com/example/xfiendx4life/schoolrate/SchoolBio.java
@@ -1,8 +1,9 @@
 package com.example.xfiendx4life.schoolrate;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import static com.example.xfiendx4life.schoolrate.SchoolCard.SCHOOL_BIO;
@@ -17,5 +18,8 @@ public class SchoolBio extends AppCompatActivity {
         String bioString = intent.getStringExtra(SCHOOL_BIO);
         TextView bio = (TextView) findViewById(R.id.bio);
         bio.setText(bioString);
+    }
+    public void close (View view) {
+        super.finish();
     }
 }
